9a3605fec6acbbe72fd7cc21bb6b344cabe7e550
Nick Bellamy <nick.bellamy@gmail.com>
2017-11-12 10:56:35 +0000

    Get the patrol points from the C++ set array
    This is the first step in converting ChooseNextWaypoint from blueprint to C++.

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,13 +1,26 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "AIController.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "PatrollingGuard.h"	// TODO Remove coupling
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	// Get the patrol points
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPoints_CPP;
+
+	// TODO Set next waypoint
+
+	// TODO Cycle index
+
+	// TODO Remove below diagnostic code log
 	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
 	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
+	
 	return EBTNodeResult::Succeeded;
 
 }
--- Source/TestingGrounds/PatrollingGuard.h
+++ Source/TestingGrounds/PatrollingGuard.h
@@ -14,6 +14,8 @@ class TESTINGGROUNDS_API APatrollingGuard : public ATP_ThirdPersonCharacter
 {
 	GENERATED_BODY()
 
+public:	
+	// TODO Create getter so PatrolPoints_CPP is not public
 	UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
 		TArray<AActor*> PatrolPoints_CPP;
 
