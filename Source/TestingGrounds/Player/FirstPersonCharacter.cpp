899750df956d70b5903f58f29ee66e4a9fcbcdce
Nick Bellamy <nick.bellamy@gmail.com>
2017-11-22 13:10:00 +0000

    Bind the "Fire" action to the OnFire() function
    This has to be done in BeginPlay() as the SetupPlayerInputComponent() overrides the Pawn's function, which means that these bindings are setup at construction.  As the gun is not constucted at this point, the binding of the Gun's OnFire() method needs to be done later, which is why it is called in BeginPlay().

--- Source/TestingGrounds/Player/FirstPersonCharacter.cpp
+++ Source/TestingGrounds/Player/FirstPersonCharacter.cpp
@@ -59,6 +59,11 @@ void AFirstPersonCharacter::BeginPlay()
 	
 	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
 	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+
+	// Bind fire event
+	// Called in the BeginPlay() method rather than from SetupPlayerInputComponent because when SetupPlayerInputComponent
+	// is called, the Gun has not yet been constructed, so the OnFire Event cannot be bound
+	InputComponent->BindAction("Fire", IE_Pressed, Gun, &AGun::OnFire);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -72,9 +77,6 @@ void AFirstPersonCharacter::SetupPlayerInputComponent(class UInputComponent* Pla
 	// Bind jump events
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
-
-	// Bind fire event
-	PlayerInputComponent->BindAction("Fire", IE_Pressed, Gun, &AGun::OnFire);
 
 	// Enable touchscreen input
 	EnableTouchscreenMovement(PlayerInputComponent);
