3be7f7036165648d2cea3d9c6c4ccb7405626411
Nick Bellamy <nick.bellamy@gmail.com>
2017-12-16 18:02:47 +0000

    Move components on BP_Character to C++
    Also move the Fire() function into the C++ Mannequin class too.

--- Source/TestingGrounds/Character/Mannequin.cpp
+++ Source/TestingGrounds/Character/Mannequin.cpp
@@ -1,21 +1,46 @@
 // Copyright Nick Bellamy.
 
 #include "Mannequin.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Weapons/Gun.h"
 
 
 // Sets default values
 AMannequin::AMannequin()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Create a CameraComponent	
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCameraComponent->RelativeLocation = FVector(2.44f, 6.75f, 74.0f); // Position the camera
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+
+	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+	Mesh1P->SetOnlyOwnerSee(true);
+	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
+	Mesh1P->bCastDynamicShadow = false;
+	Mesh1P->CastShadow = false;
+	Mesh1P->RelativeRotation = FRotator(5.2f, 1.9f, -19.2f);
+	Mesh1P->RelativeLocation = FVector(3.29f, -5.0f, -161.33f);
 }
 
 // Called when the game starts or when spawned
 void AMannequin::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	if (GunBlueprint == NULL)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Gun blueprint missing."));
+		return;
+	}
+	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
+	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint")); //Attach gun mesh component to Skeleton, doing it here because the skelton is not yet created in the constructor
+	Gun->AnimInstance = Mesh1P->GetAnimInstance();
 }
 
 // Called every frame
@@ -32,3 +57,7 @@ void AMannequin::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 
 }
 
+void AMannequin::Fire()
+{
+	Gun->OnFire();
+}
