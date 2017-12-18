5b3982f0dbc7833d782c8591188336dc505d149e
Nick Bellamy <nick.bellamy@gmail.com>
2017-12-18 19:15:58 +0000

    Implement check on whether the spawning prop is encroaching on another prop
    If encroaching, keep trying to replace the prop until 100 tries have been exhausted.

--- Source/TestingGrounds/Terrain/Tile.cpp
+++ Source/TestingGrounds/Terrain/Tile.cpp
@@ -14,35 +14,55 @@ ATile::ATile()
 }
 
 // Spawn a number of instances of specified actors (ToSpawn) between MinSpawn and MaxSpawn
-void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int32 MinSpawn, int32 MaxSpawn)
+void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int32 MinSpawn, int32 MaxSpawn, float Radius)
+{
+	// Calculate random number of actors to spawn
+	int32 NumberToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
+	for (size_t i = 0; i < NumberToSpawn; i++)
+	{
+		FVector SpawnPoint;
+		bool isFound = FindEmptyLocation(SpawnPoint, Radius);
+		if (isFound)
+		{
+			PlaceActor(ToSpawn, SpawnPoint);
+		}
+	}
+}
+
+bool ATile::FindEmptyLocation(FVector& OutLocation, float Radius)
 {
 	// Set bounds of spawn locations
 	FVector Min(0, -2000, 0);
 	FVector Max(4000, 2000, 0);
 	FBox Bounds(Min, Max);
 
-	// Calculate random number of actors to spawn
-	int32 NumberToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
-	for (size_t i = 0; i < NumberToSpawn; i++)
+	const int MAX_ATTEMPTS = 100;
+	for (size_t i = 0; i < MAX_ATTEMPTS; i++)
 	{
-		// Create random spawn point within box bounds
-		FVector SpawnPoint = FMath::RandPointInBox(Bounds);
-		// Spawn actor
-		AActor* Spawned = GetWorld()->SpawnActor<AActor>(ToSpawn);
-		// Move actor to SpawnPoint
-		Spawned->SetActorRelativeLocation(SpawnPoint);
-		// Attach spawned object to the floor
-		Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
+		FVector CandidatePoint = FMath::RandPointInBox(Bounds);
+		if (CanSpawnAtLocation(CandidatePoint, Radius))
+		{
+			OutLocation = CandidatePoint;
+			return true;
+		}
 	}
+	return false;
+}
+
+void ATile::PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint)
+{
+	// Spawn actor
+	AActor* Spawned = GetWorld()->SpawnActor<AActor>(ToSpawn);
+	// Move actor to SpawnPoint
+	Spawned->SetActorRelativeLocation(SpawnPoint);
+	// Attach spawned object to the floor
+	Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
 }
 
 // Called when the game starts or when spawned
 void ATile::BeginPlay()
 {
 	Super::BeginPlay();
-
-	CastSphere(GetActorLocation(), 300);
-	CastSphere(GetActorLocation() + FVector(0, 0, 1000), 300);
 	
 }
 
@@ -53,7 +73,7 @@ void ATile::Tick(float DeltaTime)
 
 }
 
-bool ATile::CastSphere(FVector Location, float Radius)
+bool ATile::CanSpawnAtLocation(FVector Location, float Radius)
 {
 	FHitResult HitResult;
 	bool HasHit = GetWorld()->SweepSingleByChannel(
@@ -65,6 +85,6 @@ bool ATile::CastSphere(FVector Location, float Radius)
 		FCollisionShape::MakeSphere(Radius)
 	);
 	FColor ResultColor = HasHit ? FColor::Red : FColor::Green;
-	DrawDebugCapsule(GetWorld(), Location, 0, Radius, FQuat::Identity, ResultColor, true, 100);
-	return HasHit;
+	DrawDebugCapsule(GetWorld(), Location, 0, Radius, FQuat::Identity,	 ResultColor, true, 100);
+	return !HasHit;
 }
